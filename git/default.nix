91e7cec70376d1a5cb3fe0e736fe54e3d736182f
Nicklas Sedlock <nicklas.sedlock@posteo.net>
2023-11-26 18:54:48 +0100

    feat: add `.vscode` to global gitignore
    I sometimes use VSCode for debugging and I don't really want to bother
    every time to put the `launch.json` in the `.vscode` folder onto some
    ignore.

--- git/default.nix
+++ git/default.nix
@@ -3,6 +3,7 @@
   userName = "Nicklas Sedlock";
   userEmail = "nicklas.sedlock@posteo.net";
   diff-so-fancy.enable = true;
+  ignores = [".vscode"];
   extraConfig = {
     init = {
       defaultBranch = "main";
